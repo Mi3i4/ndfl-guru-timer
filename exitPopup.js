91ec866db9a79f4d86834238e911b1730d46ee12
Mi3i4 <zelmafia@gmail.com>
2020-12-14 18:02:12 +0530

    exitPopup_classes

--- exitPopup.js
+++ exitPopup.js
@@ -2,11 +2,13 @@ class ExitPopup {
     constructor() {
         this.el = document.querySelector('#exit-popup')
         this.closeEl = document.querySelector('#close-exit-popup')
+        this.readLocalStorage = localStorage.getItem('ng_exit_popup_timer')
+
         this.closeEl.addEventListener('click', () => {
             this.closeModal()
         })
-        // this.time_end = localStorage.getItem('ng_exit_popup_time_to')
-        this.time_end = localStorage.getItem('ng_exit_popup_time_to') ?
+        this.time_end = localStorage.getItem('ng_exit_popup_time_to')
+            ?
             localStorage.getItem('ng_exit_popup_time_to') : function (){
                 const a = new Date().getTime() + (1000*10);
                 localStorage.setItem('ng_exit_popup_time_to', a);
@@ -23,29 +25,23 @@ class ExitPopup {
 
     timeLeft() {
         let current = new Date().getTime();
-        // console.log(this.time_end - current)
         return Math.floor((this.time_end - current) / 1000)
     }
 
-    // readLocalStorage() {
-    //     return localStorage.getItem('ng_exit_popup_timer')
-    // }
-
     check() {
-        const readLocalStorage = localStorage.getItem('ng_exit_popup_timer')
-        if (readLocalStorage === 'none' || readLocalStorage == undefined) {
-            if (readLocalStorage == undefined) {
-                localStorage.setItem('ng_exit_popup_timer', 'none')
-                // console.log(this.timeLeft())
-            }
-            const a = new Date().getTime() + (1000*20)
+        if (this.readLocalStorage !== 'start') {
+            localStorage.setItem('ng_exit_popup_timer', 'none')
+            // const a = new Date().getTime() + (1000*20)
             // localStorage.setItem('ng_exit_popup_time_to', a)
             localStorage.setItem('time-left', this.timeLeft())
             // this.time_end = a
-            this.openModal()
+            this.openModal();
         } else {
             if (this.timeLeft > 0) {
                 this.startTimer()
+            }
+            if(this.timeLeft == 0) {
+                this.stopTimer()
             }
             else {
                 this.openModal()
@@ -54,10 +50,9 @@ class ExitPopup {
     }
 
     openModal() {
-        const readLocalStorage = localStorage.getItem('ng_exit_popup_timer')
         const el = this.el
         document.addEventListener('mouseleave', function(cursor) {
-            if (readLocalStorage === 'none')  {
+            if (this.readLocalStorage === 'none')  {
                 if (cursor.clientY < 10){
                     el.style.display = 'block'
                 }
@@ -70,7 +65,6 @@ class ExitPopup {
         el.style.display = 'none'
         this.startTimer()
     }
-
 
     startTimer() {
         localStorage.setItem('ng_exit_popup_timer', 'start')

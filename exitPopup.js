a674452f96346445389ce819eea1efab43200610
Mi3i4 <zelmafia@gmail.com>
2020-12-16 21:20:47 +0530

    mozilla fix

--- exitPopup.js
+++ exitPopup.js
@@ -8,7 +8,7 @@ class ExitPopup {
         this.#closeEl = this.#el.querySelector(close_id);
         this.#timer = timer;
         this.#time_end = localStorage.getItem('ng_exit_popup_time_to');
-        document.addEventListener('mouseleave', () => {
+        document.addEventListener('mouseout', () => {
             this.#time_end = localStorage.getItem('ng_exit_popup_time_to');
             if (this.#timeLeft(this.#time_end) <= 0) {
                 this.#openModal();
@@ -51,4 +51,64 @@ class ExitPopup {
     }
 }
 
-ExitPopup.Init('#exit-popup', '#close-exit-popup', 5);
+// ExitPopup.Init('#exit-popup', '#close-exit-popup', 5);
+const obj = new ExitPopup('#exit-popup', '#close-exit-popup', 5);
+window.s = obj;
+
+
+// Код на webflow
+
+// <script>
+//     class ExitPopup {
+//     constructor(el, close_id, timer) {
+//     this.el = document.querySelector(el);
+//     this.closeEl = this.el.querySelector(close_id);
+//     this.timer = timer;
+//     this.time_end = localStorage.getItem('ng_exit_popup_time_to');
+//     document.addEventListener('mouseout', () => {
+//     console.log('test');
+//     this.time_end = localStorage.getItem('ng_exit_popup_time_to');
+//     if (this.timeLeft(this.time_end) <= 0) {
+//     this.openModal();
+// }
+// })
+//     if (this.time_end == undefined) {
+//     this.startTimer();
+// }
+// }
+//     /**
+//      *
+//      * @param el {string} id попака
+//      * @param close_id {string} id кнопки закрытия
+//      * @param timer {number} Время жизни таймера в секундах
+//      */
+//     static Init(el, close_id, timer) {
+//     new ExitPopup(el, close_id, timer);
+// }
+//
+//     timeLeft(time_end) {
+//     let current = new Date().getTime();
+//     return (time_end - current);
+// }
+//
+//     openModal() {
+//     this.el.style.display = 'block';
+//     this.closeEl.addEventListener('click', () => {
+//     this.closeModal();
+// })
+// }
+//
+//     closeModal() {
+//     this.el.style.display = 'none';
+//     this.startTimer();
+// }
+//
+//     startTimer() {
+//     const a = new Date().getTime() + (1000*this.timer);
+//     localStorage.setItem('ng_exit_popup_time_to', a);
+// }
+// }
+//
+//     ExitPopup.Init('#exit-popup', '#close-exit-popup', 60*60*24);
+//
+// </script>
